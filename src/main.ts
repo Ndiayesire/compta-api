8730c5aa928936663ec15e77dcf840681c1252bb
Ndiaye Sire Kane <ndiayesirekane@gmail.com>
2026-04-02 16:23:51 +0000

    remove cors temporary

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,12 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    Credentials: false,
+  });
+
   app.use('/public', express.static(join(__dirname, '..', '..', 'public')));
 
   const config = new DocumentBuilder()
